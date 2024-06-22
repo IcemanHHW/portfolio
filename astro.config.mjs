86c32da1e9dfb6c28f5caf86887036d7f4800004
Maarten <maartenkampmeijer@hotmail.com>
2024-06-22 20:02:30 +0200

    removed base because of switch to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://icemanhhw.github.io',
-  base: 'portfolio',
   integrations: [
   ]
 });
